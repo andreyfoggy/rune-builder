9e4242936a0a11d9855fb5ede98b4f8d62d409e6
andreyfoggy <andreyfoggy9009@gmail.com>
2020-06-07 16:42:50 +0300

    tmp

--- src/app/core/footer/footer.component.ts
+++ src/app/core/footer/footer.component.ts
@@ -1,12 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.less']
 })
 export class FooterComponent implements OnInit {
-  faCoffe = faCoffee;
   constructor() { }
 
   ngOnInit() {
